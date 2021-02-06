ba43f7f01aa78bb68769e3d2354d8dda3f0cacf4
Jeremy James <romarjeremy@gmail.com>
2021-02-05 22:18:08 -0500

    .

---
+++ backend/eVotingApi/Models/DTO/Responses/RegistrationResponse.cs
@@ -0,0 +1,12 @@
+﻿using eVotingApi.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eVotingApi.Models.DTO.Responses
+{
+    public class RegistrationResponse : AuthResult
+    {
+    }
+}
